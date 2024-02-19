f259ba210a56eaa484abb563d052d8310675f110
“BreezeConfirmingWms” <wmsthinksv@gmail.com>
2024-02-19 15:31:16 +0800

    参试解决gitalk不刷新问题

---
+++ platform/jsparse.py
@@ -0,0 +1,20 @@
+'''
+Author: BreezeConfirming
+Date: 2024-02-19 13:00:53
+LastEditors: “BreezeConfirmingWms” wmsthinksv@gmail.com
+LastEditTime: 2024-02-19 13:03:15
+FilePath: /NKUAI.ICU/platform/jsparse.py
+Description: 
+
+Copyright (c) 2024 by BreezeConfirming(Ming.Yan.) , All Rights Reserved. 
+'''
+import requests
+from urllib3 import HTTPResponse
+from bs4 import BeautifulSoup
+
+import scrapy
+from scrapy.cmdline import execute
+
+
+if __name__ == "__main__":
+    pass
